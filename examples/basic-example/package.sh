0062a807398e26d690d9bb3fd7ff76454aee8e4b
aries.zhou <aries.zhou@awcs.top>
2021-07-21 00:30:59 +0800

    feat: Add linux package script && Add demo guide in README.md

--- examples/basic-example/package.sh
+++ examples/basic-example/package.sh
@@ -4,28 +4,28 @@
 mvn clean install -Dmaven.test.skip=true
 
 # del example-dist
-rmdir demo-dist /s /q
+rm -fr demo-dist
 
 # create example-dist
-mkdir -p demo-dist
-mkdir demo-dist\plugins
-mkdir demo-dist\pluginConfig
+mkdir demo-dist
+mkdir -p demo-dist/plugins
+mkdir -p demo-dist/pluginConfig
 
 # copy main program and config
-cp basic-example-main\target\basic-example-main-*-exec.jar demo-dist /s /i
-cp basic-example-main\src\main\resources\application-prod.yml demo-dist /s
+cp basic-example-main/target/basic-example-main-*-exec.jar demo-dist
+cp basic-example-main/src/main/resources/application-prod.yml demo-dist
 
 # copy plugin and config
-cp plugins\basic-example-plugin1\target\*-jar-with-dependencies.jar demo-dist\plugins /s
-cp plugins\basic-example-plugin1\src\main\resources\plugin1.yml demo-dist\pluginConfig /s
+cp plugins/basic-example-plugin1/target/*-jar-with-dependencies.jar demo-dist/plugins
+cp plugins/basic-example-plugin1/src/main/resources/plugin1.yml demo-dist/pluginConfig
 
-cp plugins\basic-example-plugin2\target\*-jar-with-dependencies.jar demo-dist\plugins /s
-cp plugins\basic-example-plugin2\src\main\resources\plugin2.yml demo-dist\pluginConfig /s
+cp plugins/basic-example-plugin2/target/*-jar-with-dependencies.jar demo-dist/plugins
+cp plugins/basic-example-plugin2/src/main/resources/plugin2.yml demo-dist/pluginConfig
 
 cd demo-dist
 
 # run demo
-rename basic-example-main-*-exec.jar basic-example-start.jar
-rename application-prod.yml application.yml
+mv basic-example-main-*-exec.jar basic-example-start.jar
+mv application-prod.yml application.yml
 
 java -jar -Dspring.profiles.active=prod basic-example-start.jar
